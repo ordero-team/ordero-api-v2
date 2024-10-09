47b52cab37044fa1f67b0ccf14237ef27b5b015e
akbarsaputrait <akbarsaputrait@outlook.com>
2024-10-09 22:11:37 +0700

    Fix E2E App Testing

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,21 +1,43 @@
+import { Datasource } from '@lib/typeorm/datasource.typeorm';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
-describe('AppController (e2e)', () => {
-  let app: INestApplication;
+jest.setTimeout(30000); // 30 seconds timeout
 
-  beforeEach(async () => {
+describe('AppController (e2e)', () => {
+  let app: NestFastifyApplication;
+  let datasource: Datasource;
+
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
-    app = moduleFixture.createNestApplication();
+    app = moduleFixture.createNestApplication<NestFastifyApplication>(new FastifyAdapter());
     await app.init();
+    await app.getHttpAdapter().getInstance().ready();
+
+    try {
+      datasource = moduleFixture.get<Datasource>(Datasource);
+      await datasource.initialize();
+    } catch (error) {
+      console.error('Failed to get or initialize Datasource:', error);
+      throw error;
+    }
+  });
+
+  afterAll(async () => {
+    if (datasource) {
+      await datasource.destroy();
+    }
+    await app.close();
+    // Add a small delay to allow connections to fully close
+    await new Promise((resolve) => setTimeout(resolve, 1000));
   });
 
   it('/ (GET)', () => {
-    return request(app.getHttpServer()).get('/').expect(200).expect('Hello World!');
+    return request(app.getHttpServer()).get('/').expect(200).expect('Ordero API V2');
   });
 });
